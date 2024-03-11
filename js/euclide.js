14cde20f457cc13751711a0854425ac71ccb5d48
ChrisKABA <156420803+ChrisKABA@users.noreply.github.com>
2024-03-11 23:08:08 +0100

    Add files via upload

---
+++ js/euclide.js
@@ -0,0 +1,16 @@
+    let inputElement1 = document.getElementById("nombre1");
+    let inputElement2 = document.getElementById("nombre2");
+    let r1 = inputElement1.value;
+    let r2 = inputElement2.value;
+    // while (r2 !== 0) {
+    //   q = r1/r2;
+    //   r =  r1 - q * r2;
+    //   r = r1;
+    //   r2 = r;
+    // }
+    // return r2
+    // console.log(r2); 
+
+
+
+
